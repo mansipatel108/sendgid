565fbf0926a1953c8014ac4cc72c1391b4391493
Mansi <mpatel.y@gmail.com>
2016-03-21 21:33:04 -0400

    added sendgrid

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var sendgrid = require('sendgrid')(mansipatel108, mansipatel1996);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
  // res.render('index', { title: 'Express' });
@@ -23,9 +25,35 @@ router.get('/projects', function(req, res, next) {
 });
 
 //About get request
-router.get('/contact_me', function(req, res, next) {
-  res.render('contact_me');
+/* GET contact page. */
+router.get('/contact', function (req, res, next) {
+    req.flash('successmessage', 'Thank You. Your message has been sent.');
+    req.flash('errormessage', 'An Error has occurred.');
+    res.render('contact', { title: 'Contact', messages: null });
 });
-
+/* Email processing */
+router.post('/contact', function (req, res, next) {
+    sendgrid.send({
+        to: 'mansipatel10@yaoo.com',
+        from: req.body.email,
+        subject: 'Contact Form Submission',
+        text: "This message has been sent from the contact form at [MongoDB Demo]\r\n\r\n" +
+            "Name: " + req.body.name + "\r\n\r\n" +
+            "Phone: " + req.body.phone + "\r\n\r\n" +
+            req.body.message,
+        html: "This message has been sent from the contact form at [MongoDB Demo]<br><br>" +
+            "<strong>Name:</strong> " + req.body.name + "<br><br>" +
+            "<strong>Phone:</strong> " + req.body.phone + "<br><br>" +
+            req.body.message
+    }, function (err, json) {
+        if (err) {
+            res.status(500).json('error');
+        }
+        res.render('contact', {
+            title: 'Contact',
+            messages: req.flash('successmessage')
+        });
+    });
+});
 
 module.exports = router;
